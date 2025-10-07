4c42b13c37ea29644e6289e9a2079d7fd5c119af
WinsonL28 <winsonliang@live.com>
2025-10-07 10:43:40 -0700

    Add testing CORS from class

--- server.js
+++ server.js
@@ -1,42 +1,62 @@
-const express = require('express')
-const app = express();
-const PORT = 80;
-const message = require("./lang/messages/en/user.js");
-const utils = require('./modules/utils');
-const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const qs = require('querystring');
+const fs = require('fs').promises;
 const path = require('path');
-const cors = requier('cors')
 
-app.use(cors());
+const PORT = process.env.PORT || 3000;
 
-app.get('/COMP4537/labs/3/getDate/', (req, res) => {
-    const username = req.query.name
-    res.send(`<p style="color:blue">${message.greeting.replace("{0}", username).replace("{1}", utils.getDate())}</p>`);
-})
+const message = require('./lang/messages/en/user.js');
+const utils = require('./modules/utils');
 
-app.get('/COMP4537/labs/3/writeFile/', (req, res) => {
-    const filename = 'file.txt';
-    const text = req.query.text;
-    fs.appendFile(path.join(__dirname, filename), text, (err) => {
-        if (err) {
-            res.send(`<p style="color:red">${err}</p>`);
+
+function respond(res, statusCode, body, contentType = 'text/html; charset=utf-8') {
+    res.writeHead(statusCode, { 'Content-Type': contentType });
+    res.end(body);
+}
+
+function safePath(filename) {
+    return path.join(__dirname, path.basename(filename || ''));
+}
+
+const server = http.createServer(async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', "*");
+
+
+    const parsed = url.parse(req.url);
+    const pathname = parsed.pathname;
+    const query = qs.parse(parsed.query || '');
+
+    try {
+        if (req.method === 'GET' && pathname === '/COMP4537/labs/3/getDate/') {
+            const username = query.name || 'guest';
+            const html = `<p style="color:blue">${message.greeting.replace("{0}", username).replace("{1}", utils.getDate())}</p>`;
+            return respond(res, 200, html);
+
+        } else if (req.method === 'GET' && pathname === '/COMP4537/labs/3/writeFile/') {
+            const text = query.text || '';
+
+            await fs.appendFile('file.txt', text);
+            return respond(res, 200, `<p style="color:blue">File written successfully</p>`);
+
+        } else if (req.method === 'GET' && pathname === '/COMP4537/labs/3/readFile/') {
+            const filename = query.file;
+            if (!filename) {
+                return respond(res, 400, `<p style="color:red">Missing 'file' query parameter</p>`);
+            }
+            const filePath = safePath(filename);
+            const data = await fs.readFile(filePath, 'utf8');
+            return respond(res, 200, `<p style="color:blue">${data}</p>`);
+
         } else {
-            res.send(`<p style="color:blue">File written successfully</p>`);
+            return respond(res, 404, `<p style="color:red">Not found</p>`);
         }
-    });
-})
+    } catch (err) {
+        console.error('Server error:', err);
+        return respond(res, 500, `<p style="color:red">${String(err)}</p>`);
+    }
+});
 
-app.get('/COMP4537/labs/3/readFile/', (req, res) => {
-    const filename = req.query.file;
-    fs.readFile(path.join(__dirname, filename), 'utf8', (err, data) => {
-        if (err) {
-            res.send(`<p style="color:red">${err}</p>`);
-        } else {
-            res.send(`<p style="color:blue">${data}</p>`);
-        }
-    });
-})
-
-app.listen(PORT, '0.0.0.0', () => {
-    console.log('Running')
-})
+server.listen(PORT, () => {
+    console.log(`Server running`);
+});
